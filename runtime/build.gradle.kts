9b0f25c229c7a1f32d7a45360940a111dbf56196
magisk317 <code@mail.usdt.edu.kg>
2026-07-10 01:52:41 +0800

    refactor(logging): adopt unified XLog from smscode-core:contract
    Update smscode-core submodule to 6a31a58 which moves the canonical
    XLog into :contract. The local :runtime XLog becomes a thin delegate.
    Add explicit :smscode-core:contract dependency to :runtime.

--- runtime/build.gradle.kts
+++ runtime/build.gradle.kts
@@ -62,6 +62,7 @@ android {
 }
 
 dependencies {
+    implementation(project(":smscode-core:contract"))
     implementation(project(":smscode-core:hook"))
     implementation(project(":smscode-core:domain"))
     implementation(project(":smscode-core:runtime"))
--- runtime/src/main/java/com/github/magisk317/smscode/common/utils/XLog.kt
+++ runtime/src/main/java/com/github/magisk317/smscode/common/utils/XLog.kt
@@ -1,36 +1,19 @@
 package com.github.magisk317.smscode.common.utils
 
+import io.github.magisk317.smscode.runtime.contract.logging.LogRoute
+import io.github.magisk317.smscode.runtime.contract.logging.XLog as ContractXLog
+
+/**
+ * Thin delegate to the unified XLog in :contract.
+ * Preserves the `com.github.magisk317.smscode.common.utils.XLog` import path
+ * that existing callers use.
+ */
 object XLog {
-    @JvmStatic
-    fun v(message: String, vararg args: Any?) {
-        io.github.magisk317.smscode.xposed.utils.XLog.v(message, *args)
-    }
-
-    @JvmStatic
-    fun d(message: String, vararg args: Any?) {
-        io.github.magisk317.smscode.xposed.utils.XLog.d(message, *args)
-    }
-
-    @JvmStatic
-    fun i(message: String, vararg args: Any?) {
-        io.github.magisk317.smscode.xposed.utils.XLog.i(message, *args)
-    }
-
-    @JvmStatic
-    fun w(message: String, vararg args: Any?) {
-        io.github.magisk317.smscode.xposed.utils.XLog.w(message, *args)
-    }
-
-    @JvmStatic
-    fun e(message: String, vararg args: Any?) {
-        io.github.magisk317.smscode.xposed.utils.XLog.e(message, *args)
-    }
-
-    @JvmStatic
-    fun setLogLevel(logLevel: Int) {
-        io.github.magisk317.smscode.xposed.utils.XLog.setLogLevel(logLevel)
-    }
-
-    @JvmStatic
-    fun getLogLevel(): Int = io.github.magisk317.smscode.xposed.utils.XLog.getLogLevel()
+    @JvmStatic fun v(message: String, vararg args: Any?) = ContractXLog.v(message, *args)
+    @JvmStatic fun d(message: String, vararg args: Any?) = ContractXLog.d(message, *args)
+    @JvmStatic fun i(message: String, vararg args: Any?) = ContractXLog.i(message, *args)
+    @JvmStatic fun w(message: String, vararg args: Any?) = ContractXLog.w(message, *args)
+    @JvmStatic fun e(message: String, vararg args: Any?) = ContractXLog.e(message, *args)
+    @JvmStatic fun setLogLevel(logLevel: Int) = ContractXLog.setLogLevel(logLevel)
+    @JvmStatic fun getLogLevel(): Int = ContractXLog.getLogLevel()
 }
